86853e4c135e5ebdd402456b4ed3a81cbb36521c
Yosefi Kroytoro <81361291+sefi0609@users.noreply.github.com>
2024-11-20 16:56:49 +0200

    need to adjust

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from selenium_recaptcha_solver import RecaptchaSolver
 from selenium import webdriver
 from selenium.webdriver.edge.service import Service
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
@@ -6,16 +7,19 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from os import getenv
 
+
 options = webdriver.EdgeOptions()
-options.add_argument('--headless')  # Run in headless mode
+# options.add_argument('--headless')  # Run in headless mode
 options.add_argument('--disable-gpu')  # Disable GPU acceleration
 options.add_argument('--user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"')
 
 # Initialize Edge WebDriver
 driver = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()), options=options)
 
+solver = RecaptchaSolver(driver=driver)
+
 # Navigate to the URL
-driver.get("https://agendamentos.mne.gov.pt/en/login")
+driver.get('https://agendamentos.mne.gov.pt/en/login')
 
 # Wait for the page to load completely
 WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "body")))
@@ -35,15 +39,18 @@ password_field = driver.find_element(By.NAME, "password")  # Adjust if needed
 username_field.send_keys(getenv('avidan_email'))
 password_field.send_keys(getenv('avidan_pass'))
 
-# Here, you need to manually solve the CAPTCHA or use an external service for automation
-# As we cannot automate CAPTCHA solving in Selenium directly, you need to handle this manually or with services
+recaptcha_iframe = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, '//iframe[@title="reCAPTCHA"]'))
+)
 
-# # Optionally, if CAPTCHA is solved, submit the form (or click login button)
-# login_button = driver.find_element(By.XPATH, "//button[@type='submit']")  # Adjust if needed
-# login_button.click()
-#
-# # Wait until the page has loaded after the form submission
-# WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "body")))
+solver.click_recaptcha_v2(iframe=recaptcha_iframe)
+
+# Optionally, if CAPTCHA is solved, submit the form (or click login button)
+login_button = driver.find_element(By.XPATH, "//button[@type='submit']")  # Adjust if needed
+login_button.click()
+
+# Wait until the page has loaded after the form submission
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "body")))
 
 # Print the page source after successful login
 print(driver.page_source)
