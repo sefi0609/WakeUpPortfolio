9f0e438840d825715c37ae37d1b3b6bce1d8f8b8
sefi0609 <sefi0609@gmail.com>
2025-02-10 15:19:53 +0200

    refactor

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Streamlit apps automatically go to sleep after 24 hours of inactivity. To ensure my app stays awake, I’ve created a Python script that pings the app daily.  
 ### How It Works
 - The script runs inside a lightweight Alpine-based Docker container to remain independent of the host OS.
-- The container is deployed on AWS and is scheduled to run daily at 08:00.
+- The container is deployed on AWS ECS and is scheduled to run daily at 08:00.
 - This setup ensures the app remains responsive without manual intervention.
 
 ### Dockerfile
--- wakeup_portfolio.py
+++ wakeup_portfolio.py
@@ -4,32 +4,42 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')  # Run in headless mode
-options.add_argument('--disable-gpu')  # Disable GPU acceleration
-options.add_argument('--no-sandbox')  # Bypass OS security model (needed for Docker)
-options.add_argument('--disable-dev-shm-usage')  # Overcome limited resources in containers
-options.add_argument('--user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"')
 
-# Initialize Chrome WebDriver
-driver = webdriver.Chrome(options=options)
+def wake_up_streamlit_app() -> bool:
+    options = webdriver.EdgeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')  # Disable GPU acceleration
+    options.add_argument('--no-sandbox')  # Bypass OS security model (needed for Docker)
+    options.add_argument('--disable-dev-shm-usage')  # Overcome limited resources in containers
+    options.add_argument('--user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"')
 
-# Navigate to the URL
-driver.get('https://protfolio-yosefi-kroytoro.streamlit.app/')
+    driver = webdriver.Edge(options=options)
 
-# open a log file
-with open('logs.log', 'a') as log_file:
-    try:
-        # Wait for the button to load completely
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//button [contains(text(), "Yes, get this app back up!")]')))
+    driver.get('https://protfolio-yosefi-kroytoro.streamlit.app/')
 
-        # Find and click the button to wake up the app
-        auth_button = driver.find_element(By.XPATH, '//button [contains(text(), "Yes, get this app back up!")]')
-        auth_button.click()
+    with open('logs.log', 'a') as log_file:
+        try:
+            # Wait for the button to load completely
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//button [contains(text(), "Yes, get this app back up!")]')))
 
-        log_file.write(f"{datetime.now()} - Button clicked successfully!\n")
+            # Find and click the button to wake up the app
+            auth_button = driver.find_element(By.XPATH, '//button [contains(text(), "Yes, get this app back up!")]')
+            auth_button.click()
 
-    except Exception as e:
-        log_file.write(f"{datetime.now()} - An error occurred: {e}\n")
-    finally:
-        driver.quit()
+            log_file.write(f"{datetime.now()} - Button clicked successfully!\n")
+            return True
+
+        except Exception as e:
+            log_file.write(f"{datetime.now()} - An error occurred: {e}\n")
+            return False
+
+        finally:
+            driver.quit()
+
+
+if __name__ == "__main__":
+    ref = wake_up_streamlit_app()
+    if ref:
+        print('The Streamlit application is waking up...')
+    else:
+        print('The Streamlit application is already up')
